93456cfb8864edee4e2840db1250d3caae539308
kobaken <kentafly88@gmail.com>
2025-01-09 22:56:44 +0900

    (wip) config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
 import build from "@hono/vite-build/bun";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 
-export default defineConfig({
-	plugins: [honox(), build()],
-	resolve: {
-		alias: {
-			"@/": "/app/",
-		},
-	},
+export default defineConfig(({ mode }) => {
+	let config: UserConfig;
+	if (mode === "client") {
+		config = {
+			build: {
+				rollupOptions: {
+					input: "./app/client.ts",
+					output: {
+						dir: "./dist/static",
+						entryFileNames: "client.js",
+					},
+				},
+				copyPublicDir: false,
+			},
+		};
+	} else {
+		config = {
+			plugins: [
+				honox(),
+				build({
+					minify: false,
+				}),
+			],
+			resolve: {
+				alias: {
+					"@/": "/app/",
+				},
+			},
+		};
+	}
+	return config;
 });
