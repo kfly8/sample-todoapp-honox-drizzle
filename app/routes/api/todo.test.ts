a9868aaf972ff3f35d45e972d344988a40b2f522
kobaken <kentafly88@gmail.com>
2025-01-18 22:34:00 +0900

    Update test

--- app/routes/api/todo.test.ts
+++ app/routes/api/todo.test.ts
@@ -2,21 +2,11 @@ import { describe, expect, it } from "bun:test";
 import { testClient } from "hono/testing";
 
 import { createFakeUser } from "@/test/util";
+
 import { app } from "./RPC";
 
 describe("POST /api/todo", async () => {
 	const client = testClient(app);
-
-	it("should return 201 response", async () => {
-		const res = await client.api.todo.$post({
-			json: {
-				title: "test",
-				authorId: "unknown",
-			},
-		});
-
-		expect(res.status).toBe(500);
-	});
 
 	it("should return 201 response", async () => {
 		const user = await createFakeUser();
@@ -29,6 +19,23 @@ describe("POST /api/todo", async () => {
 		});
 
 		expect(res.status).toBe(201);
-		// TODO fetch todo and check if it's saved
+		expect(await res.json()).toEqual(
+			expect.objectContaining({
+				title: "test",
+				authorId: user.id,
+			}),
+		);
+	});
+
+	it("should return 500 response", async () => {
+		const res = await client.api.todo.$post({
+			json: {
+				title: "test",
+				authorId: "unknown", // invalid authorId
+			},
+		});
+
+		expect(res.status).toBe(500);
+		expect(await res.json()).toEqual({ message: "Failed to save todo" });
 	});
 });
