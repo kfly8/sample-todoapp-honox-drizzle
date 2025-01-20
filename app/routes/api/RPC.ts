a1755703a286123bc14fa7d9d49123bff642a5ae
kobaken <kentafly88@gmail.com>
2025-01-20 10:27:54 +0900

    Quit dynamically load

--- app/routes/api/RPC.ts
+++ app/routes/api/RPC.ts
@@ -1,34 +1,14 @@
 import { Hono } from "hono";
-import type { H } from "hono/types";
-import { filePathToPath } from "honox/utils/file";
 
-// Copy from 'honox/src/server/server.ts'
-const METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"] as const;
+import * as todo from "./todo";
+import * as todo_id from "./todo/[id]";
 
 // Create AppType for RPC.
 // This is not a routing table for the real server, just a type definition
 
-const app = new Hono();
+export const app = new Hono()
+	.basePath("/api")
+	.on("POST", "/todo", ...todo.POST)
+	.on("PATCH", "/todo/:id", ...todo_id.PATCH);
 
-const modules = {
-	"/api/todo.ts": import("./todo"),
-	"/api/todo/[id].ts": import("./todo/[id]"),
-};
-
-for (const [file, module] of Object.entries(modules)) {
-	const loaded = await module;
-	const path = filePathToPath(file);
-
-	for (const [method, handlers] of Object.entries(
-		loaded as Record<string, H[]>,
-	)) {
-		for (const m of METHODS) {
-			if (method === m) {
-				app.on(m, path, ...handlers);
-			}
-		}
-	}
-}
-
-export { app };
 export type AppType = typeof app;
