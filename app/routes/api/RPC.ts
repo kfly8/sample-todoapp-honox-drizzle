11f864bb019fc637552621fb18a6cd3675b4e307
kobaken <kentafly88@gmail.com>
2025-01-20 09:10:32 +0900

    Create RPC dynamically by Bun Glob

--- app/routes/api/RPC.ts
+++ app/routes/api/RPC.ts
@@ -1,15 +1,33 @@
+import { Glob } from "bun";
 import { Hono } from "hono";
+import type { H } from "hono/types";
+import { filePathToPath } from "honox/utils/file";
 
-// TODO: refactor
-import { POST as todo_POST } from "./todo";
-import { PATCH as todo_id_PATCH } from "./todo/[id]";
+// Copy from 'honox/src/server/server.ts'
+const METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"] as const;
+
+const glob = new Glob("**/*.{ts,tsx}");
+
+const app = new Hono().basePath("/api");
 
 // Create AppType for RPC.
 // This is not a routing table for the real server, just a type definition
 
-export const app = new Hono()
-	.basePath("/api")
-	.post("/todo", ...todo_POST)
-	.patch("/todo/:id", ...todo_id_PATCH);
+for await (const file of glob.scan("./app/routes/api/")) {
+	if (file.match(/test.(ts|tsx)$/)) continue;
 
+	const module = await import(`./${file}`);
+
+	const path = filePathToPath(file);
+
+	for (const [method, handlers] of Object.entries(module)) {
+		for (const m of METHODS) {
+			if (method === m) {
+				app.on(m, path, ...(handlers as H[]));
+			}
+		}
+	}
+}
+
+export { app };
 export type AppType = typeof app;
